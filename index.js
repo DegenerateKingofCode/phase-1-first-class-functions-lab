382ffa1536f1a802261ce0d9320b3445af2b97de
Tanner <Tanner@tleroy.com>
2022-04-04 22:21:57 -0400

    commit tree 8dcf5ee3e315b18b78941f0f68f3e6eb948f7a17

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return [drivers[0], drivers[1]]}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(time){
+    return fare => fare * time 
+
+}
+function fareDoubler(fare){
+    return fare * 2
+}
+function fareTripler(fare){
+    return fare * 3
+}
+function selectDifferentDrivers(drivers, driverFunction){
+    return driverFunction(drivers)
+}
